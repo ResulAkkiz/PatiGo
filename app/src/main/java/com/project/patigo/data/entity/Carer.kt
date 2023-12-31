32e3531c36317d9276018ab76faf38702bfdf31a
Resul Akkız <resulakkiz@gmail.com>
2023-12-31 12:58:33 +0300

    service recycler view

--- app/src/main/java/com/project/patigo/data/entity/Carer.kt
+++ app/src/main/java/com/project/patigo/data/entity/Carer.kt
@@ -1,13 +1,18 @@
 package com.project.patigo.data.entity
 
+import Comment
+
 data class Carer(
     var carerId: String,
-    var carerName: String,
-    var carerSurname: String,
+    var carerName: String,//
+    var carerSurname: String,//
     var carerAge: Int?,
-    var carerAvailableDay:List<String>,
-    var carerIban:String,
-    var carerServices:List<String>,
-    var carerProvince:String,
-
+    var carerAvailableDay: List<String>,
+    var carerIban: String,
+    var carerServices: List<String>,//
+    var carerProvince: String,//
+    var carerReviewList: List<Comment>,
+    var carerProfilePict:String,//
+    var carerInfo:String,
+    var carerPhoneNumber: String
 )
